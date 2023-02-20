0532901dbdf2651624eecc40b618d8fc0978893d
Mixail89 <Mixail@m>
2023-02-20 21:04:52 +0300

    Example03 добавил описание

--- Example03_double/Program.cs
+++ Example03_double/Program.cs
@@ -1,3 +1,7 @@
-﻿double numberA = 12;
+﻿//double - вещетвенный тип данных:может хранить не целые числа
+// int - целый типа данных:использует целые числа
+// string - может хранить от 0 до много символов от 4 байт до 2 Гб
+//bool - может хранить два значения истина ложь (true и false) 
+double numberA = 12;
 double numberB = 5;
 Console.WriteLine(numberA / numberB);
